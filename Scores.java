5f190ef3393d0f1a53460e418c8606ed384f564f
Cordell <cordellbonnieux@gmail.com>
2021-07-19 20:58:54 -0700

    Learned something new, also re-organized how Scanners are passed through the program.

--- Scores.java
+++ Scores.java
@@ -5,53 +5,58 @@ import java.util.*;
 public class Scores {
     public static void main(String[] args) throws Exception {
 
-        // user chooses from main menu
-        int choice = readChoice();
+        // create scanners
+        Scanner num = new Scanner(System.in);
+        Scanner words = new Scanner(System.in);
+        Scanner doc;
 
-        // scores file object is created
-        File file = new File("StudentScores.txt");
+        // holds user's menu choice
+        int choice = 0;
+        
+        while (choice != 4) {
 
-        // decide option
-        switch(choice) {
-            case 1: newScoresFile(file);
-                main(null);
-                break;
-            case 2: searchScores(file);
-                main(null);
-                break;
-            case 3: addScores();
-                main(null);
-                break;
-            case 4: System.exit(0);            
+            // user chooses from main menu
+            choice = readChoice(num);
+
+            // scores file object is created
+            File file = new File("StudentScores.txt");
+
+            //assign docs
+            doc = new Scanner(file);
+
+            // decide option
+            switch(choice) {
+                case 1: newScoresFile(file);
+                    break;
+                case 2: searchScores(file, words, doc);
+                    break;
+                case 3: addScores();
+                    break;
+                case 4: System.out.println("Exiting Program...");
+                    break;
+                default: System.out.println("Error: invalid selection, try again...");           
+            }
+
+            doc.close();
         }
 
+        // close the scanners
+        num.close();
+        words.close();
 
-
+        // end 
+        System.exit(0);
     }
     /**
      * Reads an integer between 1 and 4 inclusive, if the integer is outside this range the method is called again.
      * @return an integer between 1 and 4 (inclusive)
      */
-    public static int readChoice() {
+    public static int readChoice(Scanner input) {
 
         // display options - print for now
         System.out.print("Options: (1)create new scores file, (2)search the existing scores, (3)add to scores, (4)exit the program: ");
 
-        Scanner input = new Scanner(System.in);
-
-        int choice = 0;      
-
-        while (choice > 4 || choice < 1) {
-            
-            choice = input.nextInt();
-
-            System.out.println();
-
-            if (choice > 4 || choice < 1)
-                System.out.print("Error: invalid entry, try again: ");
-        }
-
-        input.close();
+        int choice = input.nextInt();
 
         return choice;
     }
@@ -82,7 +87,7 @@ public class Scores {
      * Searches a file and prints the related results
      * @return
      */
-    public static void searchScores(File file) throws Exception {
+    public static void searchScores(File file, Scanner input, Scanner scan) throws Exception {
 
         // if the file cannot be read return to main menu
         if (!file.canRead()) {
@@ -90,30 +95,30 @@ public class Scores {
             main(null);
         }
 
-        // creater a scanner obj
-        Scanner input = new Scanner(System.in);
-
         // prompt user
         System.out.print("Enter a student name or score you'd like to find:");
 
         // get a search from user
         String search = input.nextLine();
 
-        // re purpose input to scan the file 
-        input.close();
-        input = new Scanner(file);
+        System.out.println(search);
+
+        // create a scanner to read the file
+        /*Scanner scan = new Scanner(file);
 
         int numOfResults = 0;
 
-        while(input.hasNext()) {
+        while(scan.hasNext()) {
 
-            if (input.next().contains(search)) {
+            if (scan.next().contains(search)) {
                 numOfResults++;
-                System.out.printf("Result %d : %s", numOfResults, input.next());
+                System.out.printf("Result %d : %s", numOfResults, scan.next());
             }
         }
 
-        input.close();
+        scan.close();
+        */
+
     }
 
     public static void addScores() {
