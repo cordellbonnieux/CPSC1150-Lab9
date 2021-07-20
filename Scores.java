7006a87c778c3aff13b94ead2cd62ab933067cdb
Cordell <cordellbonnieux@gmail.com>
2021-07-19 19:07:54 -0700

    Created newScoresFile() method, which creates a new .txt file or wipes the current, if it exists.

--- Scores.java
+++ Scores.java
@@ -1,39 +1,82 @@
 import java.io.File; 
+import java.io.PrintWriter;
 import javax.swing.*;
 import java.util.*;
 public class Scores {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        // display options - print for now
-        System.out.println("Options: (1)create new scores file, (2)search the existing scores, (3)add to scores, (4)exit the program: ");
+        int choice = readChoice();
+
+        File file = new File("StudentScores.txt");
 
         // decide option
         switch(choice) {
-            case 1: newScores();
+            case 1: newScoresFile(file);
+                main(null);
                 break;
-            case 2: searchScores(scoresFile);
+            case 2: searchScores();
+                main(null);
                 break;
-            case 3: addScores(scoresFile);
+            case 3: addScores();
+                main(null);
                 break;
-            case 4: system.exit(0);            
+            case 4: System.exit(0);            
         }
 
 
 
     }
+    /**
+     * Reads an integer between 1 and 4 inclusive, if the integer is outside this range the method is called again.
+     * @return an integer between 1 and 4 (inclusive)
+     */
+    public static int readChoice() {
 
-    public static void newScores() {
-        // make a scores file
+        // display options - print for now
+        System.out.print("Options: (1)create new scores file, (2)search the existing scores, (3)add to scores, (4)exit the program: ");
+
+        Scanner input = new Scanner(System.in);
+
+        int choice = input.nextInt();
+
+        if (choice > 4 || choice < 1) {
+            System.out.println("Invalid entry, try again.");
+            return readChoice();
+        }
+
+        return choice;
     }
 
-    public static String[] searchScores(File scores) {
+    /**
+     * Creates a new StudentScores.txt file if one does not exist,
+     * if it does then the program wipes the data from the file.
+     * @param file a .txt file in the program's relative path
+     * @throws Exception FileNotFoundException 
+     */
+    public static void newScoresFile(File file) throws Exception {
+
+        // check for existing file
+        Boolean created = file.exists();
+
+        PrintWriter newFile = new PrintWriter(file);
+
+        if (created) 
+            System.out.println("Current scores file has been wiped");
+        else {
+            System.out.println("A new file has been created");
+        }
+
+        newFile.close();
+    }
+
+    public static String[] searchScores() {
 
         // search for score
 
         return new String[]{"hey"};
     }
 
-    public static void addScores(File scores) {
+    public static void addScores() {
         // add an entry to scores file
     }
 }
